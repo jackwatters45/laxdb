c2a084cd0d54e1148aec9b0d0ca053eda6efe542
jackwatters45 <jack.watters@me.com>
2026-01-17 23:36:06 +1100

    feat(nll-client-002): Define mapParseError helper

--- packages/pipeline/src/nll/nll.client.ts
+++ packages/pipeline/src/nll/nll.client.ts
@@ -3,3 +3,9 @@ import { Effect, type ParseResult, Schema } from "effect";
 import { makeRestClient } from "../api-client/rest-client.service";
 import { NLLConfig } from "../config";
 import { ParseError } from "../error";
+
+const mapParseError = (error: ParseResult.ParseError): ParseError =>
+  new ParseError({
+    message: `Invalid request: ${String(error)}`,
+    cause: error,
+  });
