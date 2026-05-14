8b17bcfac5578977d93da9d60fca46c12b9a52cd
jackwatters45 <jack.watters@me.com>
2026-05-15 01:12:54 +1000

    Remove fines DB facade

--- packages/fines/src/core/db.ts
+++
@@ -1,5 +0,0 @@
-import { DatabaseLiveFromBinding } from "@laxdb/core/drizzle/drizzle.service";
-
-export { DrizzleService as Db } from "@laxdb/core/drizzle/drizzle.service";
-
-export const DbLive = (d1: D1Database) => DatabaseLiveFromBinding(d1);
